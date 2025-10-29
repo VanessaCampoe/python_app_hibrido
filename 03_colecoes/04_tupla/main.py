b2b5ffe3fb794e2fe0fce4743226e887b449ca59
VanessaCampoe <vanessacampoe@gmail.com>
2025-10-29 17:17:15 -0300

    coleções

---
+++ 03_colecoes/04_tupla/main.py
@@ -0,0 +1,16 @@
+# declaracao_tupla
+dias_semana = (
+    "Domingo",
+    "Segunda-feria",
+    "Terça-feria",
+    "Quarta-feria",
+    "Quinta-feia",
+    "Sexta-feria",
+    "Sabado"
+
+
+)
+
+# saida  de dados 
+for dia in dias_semana:
+    print(dia)
